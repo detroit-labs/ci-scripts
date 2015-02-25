f5b13f6afcdbd00ae88165926bfd38583291f4f7
ctrevarthen <ct@detroitlabs.com>
2015-02-24 22:03:17 -0500

    Add more common scripts for building, distributing, and keychain access.

---
+++ iOS/common-distribute.sh
@@ -0,0 +1,22 @@
+#########################################################
+# Commonly used distribution script functions
+#########################################################
+
+function generate_build_notes() {
+  echo ${EMAIL_HEADER} > build/notes.txt
+  echo "\n" >> build/notes.txt
+  echo $(git rev-parse --abbrev-ref HEAD) >> build/notes.txt
+  echo "\n\n" >> build/notes.txt
+  echo $CHANGES_SINCE_SUCCESS >> build/notes.txt
+}
+
+function upload_build()
+{
+  ./Pods/CrashlyticsFramework/Crashlytics.framework/submit \
+    $CRASHLYTICS_API_KEY $CRASHLYTICS_BUILD_SECRET \
+    -ipaPath build/$IPA \
+    -notesPath build/notes.txt \
+    -groupAliases ${EMAIL_GROUPS}
+
+  return $?
+}
